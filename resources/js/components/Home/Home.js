4a9a5f0a557badee6d6e12dc69557cd36b6e0609
NaimBiswas <nayeembiswas2@gmail.com>
2021-03-28 13:10:06 +0600

    Add more svg incon

--- resources/js/components/Home/Home.js
+++ resources/js/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { Carousel } from 'react-bootstrap'
 import './Home.css'
+import Rocket from '../SVG/Rocket.svg'
 const Home = () => {
     return (
         <Fragment>
@@ -39,7 +40,9 @@ const Home = () => {
                                         <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                                             <div className="card" >
                                                 <div className="card-body">
-                                                    <h5 className="card-title"><div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div></h5>
+                                                    <h5 className="card-title">
+                                                        <img src={Rocket} alt="" />
+                                                    </h5>
                                                     <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                                                     <p className="card-text">
                                                         Content
